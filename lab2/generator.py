6a8c7194fc8993f414c22ad6c8b088f6270caeeb
Sebastian Olsson <sebelino@hotmail.com>
2014-02-11 16:49:38 +0100

    Ändrade strukturen på randomdata.

--- lab2/generator.py
+++ lab2/generator.py
@@ -3,4 +3,7 @@
 from random import randint
 
 def randomdata(amount,dimension=11,lowerbound=0,upperbound=20):
-    return [[randint(lowerbound,upperbound) for _ in range(dimension)] for _ in range(amount)]
+    lists = [[randint(lowerbound,upperbound) for _ in range(dimension)] for _ in range(amount)]
+    tuples = [(alist[0],alist[1:]) for alist in lists]
+    return tuples
+    
