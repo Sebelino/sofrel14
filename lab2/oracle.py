bad8bfdf03e0ecde31deb29046db4566e026cdde
Mikael Florén <mikael.floren@gmail.com>
2014-02-13 14:42:58 +0100

    More oracle test functions

--- lab2/oracle.py
+++ lab2/oracle.py
@@ -1,3 +1,5 @@
+# Determines whether the function returned the correct value
+# for a given input
 def oracle(el, alist, func):
     found = False
     for val in alist:
@@ -6,8 +8,28 @@ def oracle(el, alist, func):
             break
     return func(el, alist) == found
 
+# Tests the given function on a test suite
+# Returns the result on each test as compared to the oracle
 def testsuite(tests, func):
     results = []
     for case in tests:
         results += [oracle(case[0], case[1], func)]
+    return results
+
+# Returns the number of tests needed to find the first error
+# for a given function on a given test suite
+def firsterror(tests, func):
+    i = 0
+    for case in tests:
+        i += 1
+        if not oracle(case[0], case[1], func):
+            return i
+    return -1
+
+# Tests all functions in funcs[] on the test cases in tests[]
+# Returns the indices of the first error for each function
+def testall(tests, funcs):
+    results = []
+    for func in funcs:
+        results += [firsterror(tests, func)]
     return results
