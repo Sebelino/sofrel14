88bed42f6bd179771511931f2a7a6c46807cc636
Sebastian Olsson <sebelino@hotmail.com>
2014-02-13 14:12:05 +0100

    Pairwise är klar, med reservation för buggar.

--- lab2/pairwise.py
+++ lab2/pairwise.py
@@ -1,10 +1,25 @@
 #!/usr/local/bin/python2
 
+from pprint import pprint
+
 # Generates a pairwise test suite for typicals v with default values d.
 # pairwise([1,2,3,4],[{1,2,3},{1,2,3},{1,2,3},{1,2,3,4}]) = {[4,4,3,4],...}
-#def pairwise(defaults,typicals):
-#    len(defaults)
-    
+def pairwise(defaults,typicals):
+    result = set()
+    # First element
+    for i in range(len(typicals)):
+        v = typicals[i]
+        for element in v:
+            instance = defaults[:]
+            instance[i] = element
+            # Second element
+            for j in [x for x in range(len(typicals)) if x != i]:
+                w = typicals[j]
+                for element2 in w:
+                    newerinstance = instance[:]
+                    newerinstance[j] = element2
+                    result.add(tuple(newerinstance))
+    return result
 
 def onewise(defaults,typicals):
     result = set()
@@ -17,4 +32,4 @@ def onewise(defaults,typicals):
             result.add(tuple(instance))
     return result
 
-print(onewise([1,2,3],[{1,2,3}]*3))
+pprint(pairwise([1,2,3,4],[{1,2,3,4}]*4))
